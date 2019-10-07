449f79ffc52b48d6084ba8f59a1e5bd03f391abb
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2019-10-07 22:50:27 +0200

    Add usage example: custom Docker image

---
+++ example/custom-image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This approach is good for builds with complicated setup:
+# create a new image that runs whatever you want.
+
+# build base image with:
+#   docker build -t texlive-base-luatex --build-arg "profile=base-luatex" .
+
+docker build -t tl-hello-world .
+docker run --rm tl-hello-world:latest > hello_world.pdf
