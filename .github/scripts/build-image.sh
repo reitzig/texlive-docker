b93cfbb8a6efe7187d6dd562c33e7b917d14626f
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2019-10-26 02:44:42 +0200

    Consolidate event-triggered workflows.
    Always run tests; publish on matching tags.
    Refactor so image build script takes unfiltered revision and
    writes Docker tags to a (job-)central file.

--- .github/scripts/build-image.sh
+++ .github/scripts/build-image.sh
@@ -1,40 +1,51 @@
 #!/usr/bin/env bash
 
-set -u
+set -eu
 
-tag="${1}"
+image_tag_list="${IMAGE_TAG_LIST:-/dev/stdout}"
+
+ref="${1}"
 profile="${2:-minimal}"
 commit_time="$(date --date=@$(git show -s --format=%ct HEAD) --rfc-3339=seconds)"
     # Use commit time for (more) reproducible builds; format is required by OCI annotation spec.
 
-case "${tag}" in
-    release-* )
-        version="${tag#release-}"
+function make_docker_tag {
+    echo "reitzig/texlive-${1}:${2}"
+}
+
+case "${ref}" in
+    ref/tags/release-* )
+        version="${ref##*/release-}"
         docker build \
-            --tag reitzig/texlive-${profile}:${version} \
-            --tag reitzig/texlive-${profile}:latest \
+            --tag "$(make_docker_tag ${profile} ${version})" \
+            --tag "$(make_docker_tag ${profile} latest)" \
             --build-arg "profile=${profile}" \
             --build-arg "label_created=${commit_time}" \
             --build-arg "label_version=${version}" \
             --build-arg "label_tlversion=${version%.*}" \
             --build-arg "label_revision=$(git rev-parse --verify HEAD)" \
             .
+        make_docker_tag ${profile} ${version} >> ${image_tag_list}
+        make_docker_tag ${profile} latest >> ${image_tag_list}
         ;;
-    pre-* )
-        version="${tag#pre-}"
+    ref/tags/pre-* )
+        version="${ref##*/pre-}"
         docker build --no-cache \
-            --tag reitzig/texlive-${profile}:${version} \
+            --tag $(make_docker_tag ${profile} ${version}) \
             --build-arg "profile=${profile}" \
             --build-arg "label_created=${commit_time}" \
             --build-arg "label_version=${version}" \
             --build-arg "label_revision=$(git rev-parse --verify HEAD)" \
             .
+        make_docker_tag ${profile} ${version} >> ${image_tag_list}
         ;;
     * )
+        version="${ref##*/}"
         # This is testing, just build the thing.
         docker build --no-cache \
-            --tag texlive-${profile}:${tag} \
+            --tag $(make_docker_tag ${profile} ${version}) \
             --build-arg "profile=${profile}" \
             .
+        make_docker_tag ${profile} ${version} >> ${image_tag_list}
         ;;
 esac
